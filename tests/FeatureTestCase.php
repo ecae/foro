7906fcc837e96d751a2888d0aa2b5b48a5e9fd36
ecae11 <erickcruzestrada1993@gmail.com>
2017-04-14 11:51:33 -0500

    Add feature test case

---
+++ tests/FeatureTestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ANGGELA
+ * Date: 14/04/2017
+ * Time: 11:41
+ */
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class FeatureTestCase extends TestCase
+{
+    use DatabaseTransactions;
+}
