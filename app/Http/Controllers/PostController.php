2a601b2a3f374288bb5b54ec448fea2e86eda927
ecae11 <erickcruzestrada1993@gmail.com>
2017-04-15 22:49:24 -0500

    Class 17 for Styde.net

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function index()
+    {
+        $posts = Post::orderBy('created_at','DESC')->paginate();
+        return view('posts.index',compact('posts'));
+    }
+
     public function show (Post $post, $slug)
     {
         if($post->slug != $slug) {
