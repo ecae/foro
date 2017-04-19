c9144ec5c9b1bae48229a105d56865e66ab33ace
Erick Cruz <eric_cruz_estrada@hotmail.com>
2017-04-19 12:50:19 -0500

    Class 27 for Styde.net

--- app/User.php
+++ app/User.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
+use App\Notifications\PostCommented;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Notification;
 
 class User extends Authenticatable
 {
@@ -45,6 +47,15 @@ class User extends Authenticatable
             'post_id' => $post->id,
         ]);
         $this->comments()->save($comment);
+
+        //Notificacion cuando se escribe un comentario
+        Notification::send(
+            $post->subscribers()->where('users.id', '!=', $this->id)->get(),
+            new PostCommented($this, $comment)
+        );
+
+        return $comment;
+
     }
 
     public function createPost(array $data)
